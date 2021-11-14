03ff349101a7c909b72ce8c06d0096b8fd70acb3
baumanl <laurabauman12@gmail.com>
2021-11-13 17:40:29 -0800

    extension

--- app/src/main/java/edu/stanford/baumanl/myyelpclone/MainActivity.kt
+++ app/src/main/java/edu/stanford/baumanl/myyelpclone/MainActivity.kt
@@ -2,7 +2,11 @@ package edu.stanford.baumanl.myyelpclone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
+import android.widget.EditText
+import android.widget.SearchView
 import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -15,13 +19,35 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 private const val TAG = "MainActivity"
 private const val BASE_URL = "https://api.yelp.com/v3/"
-private const val API_KEY = "_4Fw8BhV0EymMt2jH6NbVFXW9K4mO79OCFeaN7ux83lWmFX7rD1wfiIorGuxFAnETLF6ds2ORWY-pJgPLaPPZcfqBMW5N_f1S7Vb5i9qJOGLaJ5ssPcIWN0PUSmQYXYx"
+private const val API_KEY = "API KEY"
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // val rvRestaurants = view.findViewById<RecyclerView>(R.id.rvRestaurants)
+        val searchBar = this.findViewById<SearchView>(R.id.searchbar)
+        searchBar.setOnQueryTextListener(object: SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(p0: String?): Boolean {
+                if (p0.isNullOrBlank()) {
+                    return false
+                }
+                Log.i(TAG, "new search for $p0")
+                newSearch(p0)
+                return true
+            }
+
+            override fun onQueryTextChange(p0: String?): Boolean {
+                return true
+            }
+
+        })
+
+
+
+        //
+    }
+
+    fun newSearch(term: String) {
         val restaurants = mutableListOf<YelpRestaurant>()
         val adapter = RestaurantsAdapter(this, restaurants)
         val rvRestaurants = this.findViewById<RecyclerView>(R.id.rvRestaurants)
@@ -32,7 +58,7 @@ class MainActivity : AppCompatActivity() {
         val retrofit =
             Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(GsonConverterFactory.create()).build()
         val yelpService = retrofit.create(YelpService::class.java)
-        yelpService.searchRestaurants("Bearer $API_KEY", "Avocado Toast", "New York").enqueue(object : Callback<YelpSearchResult> {
+        yelpService.searchRestaurants("Bearer $API_KEY", term, "New York").enqueue(object : Callback<YelpSearchResult> {
             override fun onResponse(call: Call<YelpSearchResult>, response: Response<YelpSearchResult>) {
                 Log.i(TAG, "onResponse: $response")
                 val body = response.body()
@@ -50,6 +76,6 @@ class MainActivity : AppCompatActivity() {
             }
 
         })
-        //
     }
+
 }
